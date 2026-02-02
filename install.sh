1beb322401741f7b4d8e1bd2df291f5d4aeb856a
Marco Riola Fraga <marcoriola@proton.me>
2026-02-02 17:25:22 +0100

    Cleaned up install script

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "Installing yay..."
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
 
 echo "Installing required packages..."
-sudo pacman -S hyprpolkitagent swww mako rofi nautilus waybar wl-clipboard git stow firefox ttf-cascadia-mono-nerd neovim matugen fastfetch zsh hypridle eza zoxide bat wiremix impala bluetui hyprlock
+sudo pacman -S hyprpolkitagent swww mako rofi nautilus waybar wl-clipboard stow firefox ttf-cascadia-mono-nerd neovim matugen fastfetch zsh hypridle eza zoxide bat impala bluetui hyprlock
 
 yay -S python-pywalfox wlogout
 
