d78b81a22c5247d7401fcaf115d5ef9918fb9d90
Marco Riola Fraga <marcoriola@proton.me>
2026-02-07 15:34:03 +0100

    Removed wlogout for rofi logout menu

--- install.sh
+++ install.sh
@@ -4,13 +4,17 @@ echo "Installing yay..."
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
 
 echo "Installing required packages..."
-sudo pacman -S hyprpolkitagent swww mako rofi nautilus waybar wl-clipboard stow firefox ttf-cascadia-mono-nerd neovim matugen fastfetch zsh hypridle eza zoxide bat impala bluetui hyprlock
+sudo pacman -S hyprpolkitagent swww mako rofi nautilus waybar wl-clipboard stow firefox ttf-cascadia-mono-nerd neovim matugen fastfetch zsh hypridle eza zoxide bat impala bluetui hyprlock wget
 
 yay -S python-pywalfox wlogout
 
 sudo pywalfox install
 
 chsh -s /usr/bin/zsh
+
+wget https://raw.githubusercontent.com/jluttine/rofi-power-menu/refs/heads/master/rofi-power-menu
+sudo cp rofi-power-menu /usr/local/bin/
+chmod +x /usr/local/bin/rofi-power-menu
 
 read -p "Do you want to remove unnecesary packages for the dotfiles? (Y/n): " choice
 
