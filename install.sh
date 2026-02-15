6949cb02125d5c2b99c033d924364b11101dbb17
Marco Riola <131653827+marcoriola@users.noreply.github.com>
2026-02-15 11:28:00 +0100

    Update install script to include ghostty and remove kitty
    Added 'ghostty' to the list of packages to install and updated the removal of 'kitty' in the cleanup section.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "Installing yay..."
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
 
 echo "Installing required packages..."
-sudo pacman -S hyprpolkitagent swww mako rofi nautilus waybar wl-clipboard stow firefox ttf-cascadia-mono-nerd neovim matugen fastfetch zsh hypridle eza zoxide bat impala bluetui hyprlock wget
+sudo pacman -S hyprpolkitagent swww mako rofi nautilus waybar wl-clipboard stow firefox ttf-cascadia-mono-nerd neovim matugen fastfetch zsh hypridle eza zoxide bat impala bluetui hyprlock wget ghostty
 
 yay -S python-pywalfox wlogout
 
@@ -12,19 +12,15 @@ sudo pywalfox install
 
 chsh -s /usr/bin/zsh
 
-wget https://raw.githubusercontent.com/jluttine/rofi-power-menu/refs/heads/master/rofi-power-menu
-sudo cp rofi-power-menu /usr/local/bin/
-chmod +x /usr/local/bin/rofi-power-menu
-
 read -p "Do you want to remove unnecesary packages for the dotfiles? (Y/n): " choice
 
 case "$choice" in
     y|Y|yes|Yes|YES)
-        sudo pacman -Rns dunst wofi dolphin
+        sudo pacman -Rns dunst wofi dolphin kitty
         ;;
     n|N|no|No|NO)
         ;;
     *)
-        sudo pacman -Rns dunst wofi dolphin
+        sudo pacman -Rns dunst wofi dolphin kitty
         ;;
 esac
